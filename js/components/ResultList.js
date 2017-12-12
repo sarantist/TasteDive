79df868b37143862e84511b56850874199127d9a
akistfs <akistfs@outlook.com>
2017-12-12 23:04:04 +0200

    replaced ListView with FlatList

--- js/components/ResultList.js
+++ js/components/ResultList.js
@@ -1,40 +1,36 @@
 import React from 'react'
-import { View, Text, TextInput, Image, ListView, TouchableHighlight} from 'react-native'
+import { View, Text, TextInput, Image, FlatList, TouchableHighlight } from 'react-native'
 import styles from '../../styles/app'
 
-const Row = (props) => {
-  const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-  
+const ListView = (props) => {
   // Navigates to detailed view
   function showDetails(data) {
-    //props.setSelectedItem(data)
     props.navigation.navigate("DetailedView", data);
   }
-
+  
   return (
-    <View style={{flex: 1}}>
-      <ListView 
-        dataSource={ds.cloneWithRows(props.appData.data)}
-        renderRow={(data, sectionID, rowID) => 
-        <TouchableHighlight onPress={() => showDetails(data)}>
-          <View style={styles.listItem}>
-            <Image
-              style={styles.listItemThumb}
-              source={{uri: 'https://img.youtube.com/vi/'+ data.yID +'/0.jpg'}}
-            />
-            <View style={styles.listItemTextContainer}>
-              <Text style={styles.listTextTitle}>
-                {data.Name}
-              </Text>
-              <Text style={styles.listText}>
-                {data.Type}
-              </Text>
-            </View>
+    <FlatList 
+      data={props.appData.data}
+      keyExtractor={(item, index) => index}
+      renderItem={({item}) => 
+      <TouchableHighlight onPress={() => showDetails(item)}>
+        <View style={styles.listItem}>
+          <Image
+            style={styles.listItemThumb}
+            source={{uri: 'https://img.youtube.com/vi/'+ item.yID +'/0.jpg'}}
+          />
+          <View style={styles.listItemTextContainer}>
+            <Text style={styles.listTextTitle}>
+              {item.Name}
+            </Text>
+            <Text style={styles.listText}>
+              {item.Type}
+            </Text>
           </View>
-        </TouchableHighlight>}
-      />
-    </View>
+        </View>
+      </TouchableHighlight>}
+    />
   )
 }
 
-export default Row;
+export default ListView;
